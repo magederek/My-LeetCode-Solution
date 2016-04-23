9565e68c2a73de06d630337aa90ffb8d2e2f7497
magederek <cghderek@gmail.com>
2016-04-23 16:22:47 +0800

    First commit locally

---
+++ LeetCode/src/cghderek/leetcode/EditDistance.java
@@ -0,0 +1,21 @@
+package cghderek.leetcode;
+
+/* 72. Edit Distance
+ * Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2.
+ * (each operation is counted as 1 step.) You have the following 3 operations permitted on a word:
+ * a) Insert a character
+ * b) Delete a character
+ * c) Replace a character
+ */
+public class EditDistance {
+	// use dynamic programming to compute the edit distance
+	public int minDistance(String word1, String word2) {
+	    return 0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
