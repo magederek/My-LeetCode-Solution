c7ddebad57fb2fe1ac1c66ffed755a7b1adba6fe
magederek <cghderek@gmail.com>
2016-04-23 19:02:37 +0800

    Finish 72. Edit Distance

--- LeetCode/src/cghderek/leetcode/EditDistance.java
+++ LeetCode/src/cghderek/leetcode/EditDistance.java
@@ -10,11 +10,53 @@ package cghderek.leetcode;
 public class EditDistance {
 	// use dynamic programming to compute the edit distance
 	public int minDistance(String word1, String word2) {
-	    return 0;
+		if(word1.isEmpty()) {
+			return word2.length();
+		}
+		if(word2.isEmpty()) {
+			return word1.length();
+		}
+		int length1 = word1.length();
+		int length2 = word2.length();
+		int m = length1 + 1;
+		int n = length2 + 1;
+		int[][] dynamicTable = new int[m][n];
+		// initialize the dynamicTable
+		for(int i = 0; i < m; i++) {
+			dynamicTable[i][0] = i;
+		}
+		for(int i = 1; i < n; i++) {
+			dynamicTable[0][i] = i;
+		}
+		// construct the matrix dynamically
+	    for(int i = 1; i < m; i++) {
+	    	for(int j = 1; j < n; j++) {
+	    		dynamicTable[i][j] = min(dynamicTable[i-1][j] + 1, // deletion
+	    				dynamicTable[i][j-1] + 1,	// insertion
+	    				dynamicTable[i-1][j-1] + (word1.charAt(i-1) == word2.charAt(j-1) ? 0 : 1)); //substitution or matches
+	    	}
+	    }
+	    return dynamicTable[length1][length2];
 	}
+	
+	private final int min(int a, int b, int c) {
+		if(a <= b) {
+			if(a <= c) {
+				return a;
+			} else {
+				return c;
+			}
+		} else if(b <= c) {
+			return b;
+		} else {
+			return c;
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EditDistance editDistance = new EditDistance();
+		System.out.println(editDistance.minDistance("abcd", "bd"));
+		System.out.println(editDistance.minDistance("abcd", "bde"));
 	}
 
 }
